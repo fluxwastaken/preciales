bb3ab0e5d164c0b51c4571234e62cedf850379e2
Marvin <msrosales@up.edu.ph>
2023-05-29 21:13:16 +0800

    Spring Files

---
+++ onlineShopPreciales/onlineShopPreciales/src/main/java/cs121mp/onlineShopPreciales/OnlineShopPrecialesApplication.java
@@ -0,0 +1,14 @@
+package cs121mp.onlineShopPreciales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopPrecialesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OnlineShopPrecialesApplication.class, args);
+	}
+	//test
+}
