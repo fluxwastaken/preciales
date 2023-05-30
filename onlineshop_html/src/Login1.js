8be9f496db1e7aa1b8ea84f4a6c812bacb1b5c94
roeypresas <rmpresas@up.edu.ph>
2023-05-30 21:56:50 +0800

    React Update NavBar

---
+++ onlineshop_html/src/Login1.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Login1.css';
+
+function Login1() {
+  return (
+    <div>
+        <div className="header">
+        <div className="headerContent">
+            <p className="headerTitle">preciales</p>
+            <div className="buttonContainer">
+            <button className="headerBtn">Sign-up</button>
+            <button className="headerBtn">Login</button>
+            </div>
+        </div>
+        </div>
+
+        <div className="body">
+            <p>welcome back!</p>
+            
+        </div>
+
+    </div>
+
+  );
+}
+
+export default Login1;
