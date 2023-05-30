8a2e6a34c411b0f45443c47e6cbf63e6006545d3
Marvin <msrosales@up.edu.ph>
2023-05-31 00:45:09 +0800

    Cross-Origin addition

--- onlineShopPreciales/onlineShopPreciales/src/main/java/cs121mp/onlineShopPreciales/controller/InfoController.java
+++ onlineShopPreciales/onlineShopPreciales/src/main/java/cs121mp/onlineShopPreciales/controller/InfoController.java
@@ -2,9 +2,10 @@ package cs121mp.onlineShopPreciales.controller;
 
 import cs121mp.onlineShopPreciales.service.InfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 public class InfoController {
 
