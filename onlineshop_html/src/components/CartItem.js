8a33ac7e705b5351a959a662ccb7f217afcb1029
eiaiae <deercia@up.edu.ph>
2023-06-14 04:59:23 +0800

    STOCK BUTTON FIXED TYL

--- onlineshop_html/src/components/CartItem.js
+++ onlineshop_html/src/components/CartItem.js
@@ -27,14 +27,14 @@ function CartItem(props) {
     console.log('Stock added:', stockValue * 1);
     // Make the API call or perform any other desired action
     try {
-      await fetch(`http://localhost:8080/updateCart?buyer_id=${cart.buyer_id}&product_id=${cart.product_id}&quantity=${cart.quantity + (stockValue * 1)}`,{
+      await fetch(`http://localhost:8080/updateCart?buyer_id=${cart.buyer_id}&product_id=${cart.product_id}&quantity=${cart.quantity + (stockValue * 1)}`, {
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         method: "PUT"
       })
-      
+
     } catch (error) {
       console.error(error)
     }
@@ -42,26 +42,26 @@ function CartItem(props) {
     setStockValue(''); // Clear the input field after adding stock
   };
 
-  const handleDecreaseStock = async() => {
+  const handleDecreaseStock = async () => {
     console.log('Stock decreased:', stockValue * -1);
-    if (cart.quantity +(stockValue * -1) <= 0) {
+    if (cart.quantity + (stockValue * -1) <= 0) {
       await fetch(`http://localhost:8080/deleteCart/${cart.cart_id}`)
       window.location.reload()
     } else {
-       // Make the API call or perform any other desired action
-     try {
-      await fetch(`http://localhost:8080/updateCart?buyer_id=${cart.buyer_id}&product_id=${cart.product_id}&quantity=${cart.quantity + (stockValue * -1)}`,{
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "PUT"
-      })
-      
-    } catch (error) {
-      console.error(error)
-    }
-    window.location.reload()
+      // Make the API call or perform any other desired action
+      try {
+        await fetch(`http://localhost:8080/updateCart?buyer_id=${cart.buyer_id}&product_id=${cart.product_id}&quantity=${cart.quantity + (stockValue * -1)}`, {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "PUT"
+        })
+
+      } catch (error) {
+        console.error(error)
+      }
+      window.location.reload()
     }
     setStockValue(''); // Clear the input field after adding stock
   };
@@ -72,30 +72,32 @@ function CartItem(props) {
       <Card className="product-card">
         <Card.Img src={cart.picture} alt={cart.product_name} className='product-card-image' />
         <Card.Body>
-          <div className = "stockContext">
+          <div className="stockContext">
             {/* <Card.Title className="product-card-title">Buyer ID: {cart.buyer_id}</Card.Title>
             <Card.Text className="product-card-title">Product ID: {cart.product_id}</Card.Text> */}
             <Card.Title className="product-card-title">{cart.product_name}</Card.Title>
             <Card.Text className="product-card-text price">PHP {cart.price}</Card.Text>
             <Card.Text className="product-card-text">Quantity in Cart: {cart.quantity}</Card.Text>
-          
-        
-          <div className="stock-input-container">
-            <Button className="add-stock-button" onClick={handleDecreaseStock}>-</Button>
-            <input 
-            type="text"
-            value={stockValue}
-            onChange={handleStockChange}
-            className="stock-input"
-            placeholder="Quantity"/>
-            <Button className="add-stock-button" onClick={handleAddStock}>+</Button>
-          </div>
+
+
+            <div className="stock-input-container">
+              <div className="stock-input-wrapper">
+                <Button className="add-stock-button" onClick={handleDecreaseStock}>-</Button>
+                <input
+                  type="text"
+                  value={stockValue}
+                  onChange={handleStockChange}
+                  className="stock-input"
+                  placeholder="Quantity" />
+                <Button className="add-stock-button" onClick={handleAddStock}>+</Button>
+              </div>
+            </div>
           </div>
           <Button className="product-card-button" onClick={handleDeleteFromCart}>{buttonText}</Button>
         </Card.Body>
       </Card>
     </div>
-    
+
   );
 }
 
