02f1c34d6f4a63b995803c19004bd9ab579effad
Marvin Andrew Rosales <msrosales@up.edu.ph>
2024-06-11 07:39:40 +0000

    Revert

--- onlineshop_html/src/Signup.js
+++ onlineshop_html/src/Signup.js
@@ -152,7 +152,7 @@ const Signup = () => {
 
       <div className="body">
         <div className="welcome-container">
-          <p className="welcome">SIGN-UP</p>
+          <p className="welcome">hey there, beautiful!</p>
         </div>
 
         <div className="tableContainer">
